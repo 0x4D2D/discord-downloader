bf568f543322600c823e2f0c7acc9661b446a830
Saturn5Vfive <58058856+Saturn5Vfive@users.noreply.github.com>
2022-09-05 03:21:15 -0500

    update chatview

--- chatview.py
+++ chatview.py
@@ -3,15 +3,19 @@ import json
 file = input("File to Load~")
 write = input("File to write parsed output to? (leave blank for none) ~")
 
-buf = ""
+buf = "--START OF CHAT LOGS--\n"
 
 with open(file, "r") as handle:
     obj = json.loads(handle.read())
     messages = obj['messages']
     for message in messages:
-        buf += f"{message['author']['username']}: {message['content']}\n"
+        buf = buf + f"{message['author']['username']}: {message['content']}\n"
         print(f"{message['author']['username']}: {message['content']}")
 
+buf = buf + "--END OF CHAT LOGS--\n"
+
 if len(write) > 0:
-    with open(write, "w+") as handle:
+    with open(write, "w+", encoding='utf-8') as handle:
         handle.write(buf)
+
+input()
